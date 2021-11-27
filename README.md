e895524c4af3b646f5237550662e3b54cbddd297
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2021-11-27 21:59:22 +0200

    error handlings

--- README.md
+++
@@ -1,3 +0,0 @@
-# node-templating-engines
-# node_first_pet_project_from_udemy
-# node_first_pet_project_from_udemy
--- app.js
+++ app.js
@@ -46,10 +46,15 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then((user) => {
+      if (!user) {
+        return next();
+      }
       req.user = user;
       next();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      next(new Error(err));
+    });
 });
 
 app.use((req, res, next) => {
@@ -62,7 +67,14 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  res.redirect('/500');
+});
 
 mongoose
   .connect(MONGODB_URI)
